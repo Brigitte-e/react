a8126ee5997d30e380d9c0ce40ca1c3c98ef7936
Anastasiya Khymko <anastasiya.khymko@gmail.com>
2018-01-09 10:38:55 +0200

    init

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        Hello from Home
+      </div>
+    );
+  }
+}
+
+export default Home;
