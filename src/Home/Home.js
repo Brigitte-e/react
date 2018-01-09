711cd9ece8a97535c0d2a196d7afe9cbfb2d2a0f
Anastasiya Khymko <anastasiya.khymko@gmail.com>
2018-01-09 11:45:29 +0200

    new components and routing

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './../logo.svg';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        Hello from Home
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to My Home Page</h1>
+        </header>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Home from './Home/Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
+
+render(
+  <BrowserRouter  path="/" component={App}>
+    <div>
+      <Route path="/home" component={Home} />
+    </div>
+  </BrowserRouter>,
+
+  document.getElementById('root')
+)
