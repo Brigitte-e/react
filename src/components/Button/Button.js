9a6e06f9cb8a1b92b9948db4ea7f412594833948
Anastasiya Khymko <anastasiya.khymko@gmail.com>
2018-01-10 11:54:02 +0200

    states & events

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,31 +2,32 @@ import React, { Component } from 'react';
 import './Button.scss';
 
 class Button extends Component {
-  // colorClass = '';
-  // isActive = false;
 
   constructor(props) {
     super(props);
-    // this.isActive = this.isActive.bind(this);
-    // this.isActive = false;
-    this.showMessage = this.showMessage.bind(this);
-    // this.colorClass = this.colorClass.bind(this);
+    this.state = {
+      isToggleOn: true
+    };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  showMessage() {
-    console.log('Clicked');
-    // this.isActive = !this.isActive;
-    // if(this.isActive)
-    //   this.colorClass = this.colorClass.push('pink');
-    //   console.log('this.colorClass',this.colorClass);
+  handleClick() {
+    // this.setState(prevState => ({
+    //   isToggleOn: !prevState.isToggleOn
+    // }));
+    //or
+    this.setState({
+      isToggleOn: !this.state.isToggleOn
+    });
   }
 
   render() {
     return (
-      // <div className={'button ' + this.colorClass}>
-      <div className='button '>
-        // {this.colorClass}here
-        <button onClick={this.showMessage}>Click me</button>
+      <div className={'button ' + (this.state.isToggleOn ? '' : 'active')}>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
       </div>
     );
   }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,39 @@ import News from './../News/News';
 import Button from './../Button/Button';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+// shouldComponentUpdate - должен ли компонент обновиться? На самом деле, обычно реакт сам отлично разбирается. Но иногда ручное управление позволяет существенно ускорить работу в "узких местах". С этим методом нужно работать очень аккуратно.
+// componentWillUpdate - вызывается прямо перед render, когда новые props и state получены. В этом методе нельзя вызывать setState.
+// componentDidUpdate - вызывается сразу после render. Не вызывается в момент первого render'а компонента.
+// componentWillUnmount - вызывается сразу перед тем, как компонент будет удален из DOM.
+// componentWillMount - компонент будет примонтирован. В данный момент у нас нет возможности посмотреть DOM элементы.
+// componentDidMount - компонент примонтировался.
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  //------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     let my_news = [
       {
@@ -29,6 +62,7 @@ class Home extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to My Home Page</h1>
         </div>
+         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         <News news={my_news} />
         <Button />
       </div>
